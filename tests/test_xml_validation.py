e2a6c9afeb5faa8205ce4777e011fa7055c82e90
Christian Federmann <chrife@microsoft.com>
2019-05-30 12:27:21 -0700

    Updated test_can_import_xmlschema()

--- tests/test_xml_validation.py
+++ tests/test_xml_validation.py
@@ -7,7 +7,9 @@ import unittest
 class TestXMLValidation(unittest.TestCase):
     """Tests XML validation code."""
     def test_can_import_xmlschema(self):
+        """Checks if we can import xmlschema"""
         import xmlschema
+        self.assertEqual(xmlschema.__name__, 'xmlschema')
 
 
 if __name__ == "__main__":
