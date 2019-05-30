3d92717d66956be01e9ca60a8711aa6a6c050134
Christian Federmann <chrife@microsoft.com>
2019-05-30 11:53:05 -0700

    Added valitest.py

---
+++ valitest.py
@@ -0,0 +1,16 @@
+"""
+valitest: validatable test sets for machine translation
+"""
+
+import xmlschema
+
+
+class ValidatableTestSet:
+    """
+    Test set for machine translation, which can be validated.
+
+    Based on SGML format used at the Conference on Machine Translation
+    (WMT; see, e.g., http://statmt.org/wmt19/) but adding XSD schema for
+    validation of test sets and corresponding translation outputs.
+    """
+    raise NotImplementedError('You know what to do...')
