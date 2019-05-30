f0e3e932e14fcd4d62a35d48961bb1f3401b3b9c
Christian Federmann <chrife@microsoft.com>
2019-05-30 15:10:26 -0700

    Updated test_xml_validation()

--- tests/test_xml_validation.py
+++ tests/test_xml_validation.py
@@ -2,14 +2,26 @@
 valitest: validatable test sets for machine translation
 """
 import unittest
+import urllib
+import xmlschema
 
 
 class TestXMLValidation(unittest.TestCase):
     """Tests XML validation code."""
+
     def test_can_import_xmlschema(self):
         """Checks if we can import xmlschema"""
-        import xmlschema
         self.assertEqual(xmlschema.__name__, 'xmlschema')
+
+    def test_can_load_xsd_schema(self):
+        """Checks if we can load XSD schema"""
+        schema = xmlschema.XMLSchema('valitest.xsd')
+        self.assertIsInstance(schema, xmlschema.XMLSchema)
+
+    def test_cannot_load_invalid_file(self):
+        """Checks if we raise for invalid file"""
+        with self.assertRaises(urllib.error.URLError):
+            _ = xmlschema.XMLSchema('does-not-exist.xsd')
 
 
 if __name__ == "__main__":
