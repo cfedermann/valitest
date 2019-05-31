91f0d0cee62395d9e44208c3056737602750aa1e
Christian Federmann <chrife@microsoft.com>
2019-05-30 19:37:03 -0700

    Updated tests

---
+++ tests/context.py
@@ -0,0 +1,14 @@
+"""
+valitest: validatable test sets for machine translation
+"""
+
+# From: [The Hitchhiker's Guide to Python](https://docs.python-guide.org/writing/structure/)
+import os
+import sys
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+)
+
+# pylint: disable-msg=wrong-import-position,unused-import
+from valitest import valitest
--- tests/test_xml_validation.py
+++ tests/test_xml_validation.py
@@ -3,7 +3,15 @@ valitest: validatable test sets for machine translation
 """
 import unittest
 import urllib
+import xml
+
+from os import getcwd
+from os.path import join
+
 import xmlschema
+
+# pylint: disable-msg=no-name-in-module
+from context import valitest
 
 
 class TestXMLValidation(unittest.TestCase):
@@ -22,6 +30,14 @@ class TestXMLValidation(unittest.TestCase):
         """Checks if we raise for invalid file"""
         with self.assertRaises(urllib.error.URLError):
             _ = xmlschema.XMLSchema('does-not-exist.xsd')
+
+    def test_does_not_validate_invalid_sgml_file(self):
+        """Checks if we raise for invalid SGML file"""
+        with self.assertRaises(xml.etree.ElementTree.ParseError):
+            sgml_file = join(
+                getcwd(), 'testdata', 'newstest2019-defr-src-ts.de.sgm'
+            )
+            _ = valitest.ValidatableTestSet(sgml_file)
 
 
 if __name__ == "__main__":
